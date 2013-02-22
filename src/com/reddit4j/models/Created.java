94fb236c775101a574fe08f3e21c500ca84c6a08
Samuel Karp <sam@samuelkarp.com>
2013-02-22 00:06:40 -0800

    Refactor VotableCreated to extend Created

--- src/com/reddit4j/models/Created.java
+++ src/com/reddit4j/models/Created.java
@@ -1,20 +1,32 @@
 package com.reddit4j.models;
 
+import org.codehaus.jackson.annotate.JsonProperty;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 
 public abstract class Created extends RedditObject implements CreatedInterface {
 
-	private DateTime created;
-	private DateTime createdUtc;
+    private DateTime created;
 
-	@Override
-	public DateTime getCreated() {
-		return created;
-	}
+    @JsonProperty("created_utc")
+    private DateTime createdUtc;
 
-	@Override
-	public DateTime getCreatedUtc() {
-		return createdUtc;
-	}
+    protected void setCreated(long seconds) {
+        this.created = new DateTime(seconds * 1000, DateTimeZone.UTC);
+    }
+
+    protected void setCreatedUtc(long seconds) {
+        this.createdUtc = new DateTime(seconds * 1000, DateTimeZone.UTC);
+    }
+
+    @Override
+    public DateTime getCreated() {
+        return created;
+    }
+
+    @Override
+    public DateTime getCreatedUtc() {
+        return createdUtc;
+    }
 
 }
--- src/com/reddit4j/models/VotableCreated.java
+++ src/com/reddit4j/models/VotableCreated.java
@@ -1,29 +1,13 @@
 package com.reddit4j.models;
 
-import org.codehaus.jackson.annotate.JsonProperty;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 
-public abstract class VotableCreated extends RedditObject implements CreatedInterface, VotableInterface {
+public abstract class VotableCreated extends Created implements VotableInterface {
 
     private int ups;
 
     private int downs;
 
     private Boolean likes;
-
-    private DateTime created;
-
-    @JsonProperty("created_utc")
-    private DateTime createdUtc;
-
-    public void setCreated(long seconds) {
-        this.created = new DateTime(seconds * 1000, DateTimeZone.UTC);
-    }
-
-    public void setCreatedUtc(long seconds) {
-        this.createdUtc = new DateTime(seconds * 1000, DateTimeZone.UTC);
-    }
 
     @Override
     public int getUps() {
@@ -38,16 +22,6 @@ public abstract class VotableCreated extends RedditObject implements CreatedInte
     @Override
     public Boolean getLikes() {
         return likes;
-    }
-
-    @Override
-    public DateTime getCreated() {
-        return created;
-    }
-
-    @Override
-    public DateTime getCreatedUtc() {
-        return createdUtc;
     }
 
 }
