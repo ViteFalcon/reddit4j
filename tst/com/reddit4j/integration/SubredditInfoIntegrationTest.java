7c2623dfe1f3a0f0faf5582facfd6b9e8f2306f7
Samuel Karp <sam@samuelkarp.com>
2013-04-08 01:21:41 -0700

    Add integration testing support and basic integration tests around subreddit info

---
+++ tst/com/reddit4j/integration/SubredditInfoIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.reddit4j.integration;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.apache.http.HttpException;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.junit.Test;
+
+import com.reddit4j.clients.RedditClient;
+import com.reddit4j.models.Subreddit;
+
+public class SubredditInfoIntegrationTest {
+
+    private RedditClient client = new RedditClient();
+
+    /*
+     * Since these are integration tests, we can only assert against values we
+     * are relatively certain will not change.
+     */
+
+    @Test
+    public void testPublicDefault_Pics() throws JsonParseException, JsonMappingException, HttpException, IOException {
+        Subreddit pics = client.getSubredditInfo("pics");
+        assertEquals("/r/pics/", pics.getUrl());
+        assertEquals("2qh0u", pics.getId());
+    }
+
+    @Test
+    public void testPublic_Java() throws JsonParseException, JsonMappingException, HttpException, IOException {
+        Subreddit java = client.getSubredditInfo("java");
+        assertEquals("/r/java/", java.getUrl());
+        assertEquals("2qhd7", java.getId());
+    }
+}
