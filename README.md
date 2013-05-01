31d8b87c520e86ae884acdae1509f21f48ee9098
Samuel Karp <sam@samuelkarp.com>
2013-04-30 22:02:19 -0700

    Add integration test runtime properties groundwork

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-reddit4j
+# reddit4j
 
+## About
 Reddit4j is an unofficial Java library for the Reddit API.  Reddit4j is a work-in-progress and is not yet ready for consumption.
 
 [![Build Status](https://travis-ci.org/reddit4j/reddit4j.png?branch=master)](https://travis-ci.org/reddit4j/reddit4j)
 
-Dependencies:
+## Dependencies
 * Apache Ant
 * Apache HttpComponents HttpClient
 * Apache Commons Lang
@@ -12,3 +13,6 @@ Dependencies:
 * Junit
 * Mockito
 * Slf4j
+
+## Integration testing
+Automated integration testing is not enabled with Travis at present, as storing account credentials in the clear is somewhat undesirable.  To integration test reddit4j, please create an integration.properties file in the tst/com/reddit4j/integration directory.  A sample integration.properties.example is provided.
--- tst/com/reddit4j/integration/IntegrationRuntime.java
+++ tst/com/reddit4j/integration/IntegrationRuntime.java
@@ -1,15 +1,42 @@
 package com.reddit4j.integration;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import com.reddit4j.clients.RedditClient;
 
 public class IntegrationRuntime {
-    private static RedditClient client = new RedditClient("reddit4j integration test runner - 0.01");
+    private static RedditClient client;
+    private static Properties properties;
 
     private IntegrationRuntime() {
     }
 
     public static RedditClient getClient() {
+        // lazy initialize, since unit tests running alone don't need this
+        if (client == null) {
+            client = new RedditClient("reddit4j integration test runner - 0.01");
+        }
         return client;
+    }
+
+    public static Properties getProperties() {
+        // lazy initialize, since unit tests running alone don't need this
+        if (properties == null) {
+            properties = new Properties();
+            InputStream propertyStream = IntegrationRuntime.class.getResourceAsStream("integration.properties");
+            try {
+                properties.load(propertyStream);
+                propertyStream.close();
+            } catch (IOException e) {
+                // re-throw as a runtime exception so we don't have to
+                // explicitly catch IOException every time we want to get
+                // properties for integration tests
+                throw new RuntimeException("Could not read integration.properties", e);
+            }
+        }
+        return properties;
     }
 
 }
